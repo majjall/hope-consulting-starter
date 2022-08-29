f2a283facb7eb0f9d2ba4884b893b40444007e01
jalloh <joel.alloh>
2022-08-29 04:50:21 +0200

    Changed code-first grapghql generated file location to src folder

---
+++ servers/nestjs-prisma-server/src/core/config/config.ts
@@ -0,0 +1,43 @@
+import type { Config } from './config.interface';
+
+// export default (): Config => config;
+// export const configuration = (): Config => ({});
+// export const configuration = (): Config => (config);
+export const configuration = (): Config => {
+  const config: Config = {
+    NODE_ENV: process.env.NODE_ENV,
+    nest: {
+      port: Number(process.env.PORT) || 3000, // parseInt(process.env.PORT, 10),
+      isDevelopment: process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === '',
+      apiPath: process.env.API_PATH || 'api',
+      // prefix: 'api',
+      // host: process.env.HOST,
+    },
+    cors: {
+      enabled: Boolean(process.env.CORS_ENABLED),
+    },
+    swagger: {
+      enabled: Boolean(process.env.SWAGGER_ENABLED),
+      title: process.env.SWAGGER_TITLE || 'Nestjs API Documentation',
+      description: process.env.SWAGGER_DESCRIPTION || 'The NestJs API description',
+      version: process.env.SWAGGER_VERSION || '1.0.0',
+      path: process.env.SWAGGER_PATH || 'api/docs',
+      tag: 'REST api',
+    },
+
+    // Graphql
+    graphql: {
+      playgroundEnabled: Boolean(process.env.GRAPHQL_PLAYGROUND),
+      debug: Boolean(process.env.GRAPHQL_DEBUG),
+      schemaDestination: process.env.GRAPHQL_CODE_FIRST_SCHEMA_DESTINATION,
+      sortSchema: Boolean(process.env.GRAPHQL_SORT_SCHEMA),
+      // uploads: {
+      //   maxFileSize: 20_000_000, // 20 MB
+      //   maxFiles: 5
+      // },
+      // tracing: false,
+    },
+  };
+  // console.log(JSON.stringify(config));
+  return config;
+};
